962f9d8fe6d9a002a2287e0fc14a840d1b4b2e2d
gantar22 <nathancarterwilliams@gmail.com>
2024-09-29 20:36:26 -0400

    laboro

--- kartero_rubaĵo/Kuriero.cs
+++ kartero_rubaĵo/Kuriero.cs
@@ -1,33 +1,107 @@
 using Godot;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 public partial class Kuriero : Node2D
 {
 	[Export] private float _moveSpeed = 100f;
+
+	[Signal]
+	public delegate void OnHitPlayerEventHandler();
 	
 	private Vector2 _moveDir;
 
 	private SceneTreeTimer _turnTimer;
+
+	private Player _target;
+
+	public enum State
+	{
+		Passive,
+		Charge,
+		Attack,
+	}
+
+	public State state = State.Passive;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override async void _Ready()
 	{
-		Visible = false;
-		await ToSignal(GetTree().CreateTimer(GD.RandRange(3f, 12f)),Timer.SignalName.Timeout);
-		Visible = true;
 		_turnTimer = GetTree().CreateTimer(GD.RandRange(3f, 5f));
 		_moveDir = new Vector2(GD.Randf() > .5f ? 1f : -1f,1f).Normalized();
-		TurnRoutine();
+		// bizara magiko: sen rulilo, la asynka funkcia ruliĝas
+	}
+
+	public async void Init(Player _player)
+	{
+		_target = _player;
+		var _ = TurnRoutine();
 	}
 
 	private async Task TurnRoutine()
 	{
 		while (true)
 		{
-			await ToSignal(_turnTimer, Timer.SignalName.Timeout);
-			_turnTimer = GetTree().CreateTimer(GD.RandRange(2f, 5f));
-			_moveDir = new Vector2(GD.Randf() > .5f ? 1f : -1f,1f).Normalized();
+			// await ToSignal(_turnTimer, Timer.SignalName.Timeout);
+			// _turnTimer = GetTree().CreateTimer(GD.RandRange(2f, 5f));
+			// _moveDir = new Vector2(GD.Randf() > .5f ? 1f : -1f,1f).Normalized();
+			var attackAngle = (_target.Position - Position).Normalized();
+			var dirs = new[]
+			{
+				new Vector2( 1, 1),
+				new Vector2( 1,-1),
+				new Vector2(-1, 1),
+				new Vector2(-1,-1)
+			};
+			var orderedDirs = dirs.OrderBy(_ => Mathf.Abs(_.AngleTo(attackAngle))).ToArray();
+			var desiredDir = orderedDirs.Skip(1).FirstOrDefault().Normalized();
+			var closestDir = orderedDirs.FirstOrDefault().Normalized();
+			if (Mathf.Abs(closestDir.AngleTo(attackAngle)) < .1f)
+			{
+				await ToSignal(GetTree().CreateTimer(.2), Timer.SignalName.Timeout);
+				// charge up an attack
+				state = State.Charge;
+				_moveDir = new Vector2();
+				var line = (Line2D)GetNode("Line2D");
+				line.Points = new Vector2[2];
+				line.Points[0] = new Vector2();
+				line.Points[1] = new Vector2();
+				float length = 0f;
+				while (length < Mathf.Max(_target.Position.DistanceTo(Position),200f) && GetViewportRect().HasPoint(Position + attackAngle * length))
+				{
+					line.SetPointPosition(1,attackAngle * length);
+					length += (float)GetProcessDeltaTime() * 475f;
+					await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+				}
+
+				await ToSignal(GetTree().CreateTimer(.125), Timer.SignalName.Timeout);
+				
+				// release
+				state = State.Attack;
+				var alpha = 0f;
+				Vector2 start = Position;
+				Vector2 end = Position + attackAngle * length;
+				while (alpha <= 1f)
+				{
+					alpha += (float)GetProcessDeltaTime() * 4.5f;
+					Position = start.Lerp(end, alpha);
+					line.SetPointPosition(1,end - Position);
+					await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+				}
+				
+				line.Points = Array.Empty<Vector2>();
+				// end
+				state = State.Passive;
+				await ToSignal(GetTree().CreateTimer(.75), Timer.SignalName.Timeout);
+			}
+			_moveDir = desiredDir;
+			if (!GetViewportRect().HasPoint(Position + _moveDir * 100f))
+			{
+				_moveDir = dirs.MinBy(_=>Mathf.Abs(_.AngleTo(GetViewportRect().GetCenter() - Position)));
+				await ToSignal(GetTree().CreateTimer(.5), Timer.SignalName.Timeout);
+			}
+			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
 		}
 	}
 
@@ -49,7 +123,16 @@ public partial class Kuriero : Node2D
 				_moveDir.Y *= -1;
 			}
 		}
+		var speed = _moveSpeed * (state == State.Attack ? 20 : 1);
 		poz += (float)delta * _moveDir * _moveSpeed;
 		SetPosition(poz);
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		if (body.Owner is Player player)
+		{
+			EmitSignal(SignalName.OnHitPlayer);
+		}
 	}
 }
--- kartero_rubaĵo/Provo.cs
+++ kartero_rubaĵo/Provo.cs
@@ -1,15 +1,88 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 public partial class Provo : Node2D
 {
+	[Export]
+	public PackedScene TuroScene { get; set; }
+	
+	[Export]
+	public PackedScene KurieroScene { get; set; }
+	
+	private double _playerHealth = 100;
+
+	private HashSet<Kuriero> _vivajKurieroj = new HashSet<Kuriero>();
+
+	private Player _turo = null;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_turo = (Player)TuroScene.Instantiate();
+		_turo.Position = GetViewportRect().Position + .5f * GetViewportRect().Size;
+		AddChild(_turo);
+		var _ = _SpawnRoutine();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		ProgressBar Sano = (ProgressBar)GetNode("CanvasLayer/Sano");
+		Sano.Value = _playerHealth;
+		ProgressBar Energio = (ProgressBar)GetNode("CanvasLayer/Energio");
+		Energio.Value = _turo.energio * 100;
+	}
+
+	private async Task _SpawnRoutine()
+	{
+		await ToSignal(GetTree().CreateTimer(2.5), Timer.SignalName.Timeout);
+		while (true)
+		{
+			List<Task> tasks = new List<Task>();
+			Rect2 viewport = GetViewportRect();
+			Rect2 zone = new Rect2();
+			{
+				zone.Position = new Vector2((float)GD.RandRange(0,viewport.Size.X),(float)GD.RandRange(0,viewport.Size.Y));
+				zone.End = new Vector2((float)GD.RandRange(zone.Position.X,viewport.Size.X),(float)GD.RandRange(zone.Position.Y,viewport.Size.Y));
+			};
+			tasks.Add(_SpawnOneKuriero(zone));
+			while (tasks.Any(_=>!_.IsCompleted))
+			{
+				await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+			}
+			await ToSignal(GetTree().CreateTimer(1), Timer.SignalName.Timeout);
+		}
+	}
+
+	private async Task _SpawnOneKuriero(Rect2 Zone)
+	{
+		while (Zone.HasPoint(_turo.Position))
+		{
+			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		}
+		
+		var malamiko = (Kuriero)KurieroScene.Instantiate();
+		AddChild(malamiko);
+		_vivajKurieroj.Add(malamiko);
+		malamiko.Position = Zone.Position + .5f * Zone.Size;
+		malamiko.Init(_turo);
+		bool done = false;
+		malamiko.OnHitPlayer += () =>
+		{
+			if (malamiko.state == Kuriero.State.Attack)
+			{
+				_playerHealth -= 10;
+			} else if (_turo.state != Player.State.Attack)
+			{
+				_playerHealth -= 5;
+			}
+			_vivajKurieroj.Remove(malamiko);
+			malamiko.QueueFree();
+			done = true;
+		};
+		await ToSignal(malamiko, Node.SignalName.TreeExiting);
 	}
 }
